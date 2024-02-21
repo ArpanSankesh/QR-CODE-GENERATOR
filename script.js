d64f1e9d20cede1ff02de29adc990e79a0441aa9
ArpanSankesh <arpansankesh830@gmail.com>
2024-02-21 22:23:38 +0530

    it is working

--- script.js
+++ script.js
@@ -6,17 +6,20 @@ let Image = document.querySelector('#Image');
 
 
 let generateQr = function (){
-    if(inpBox.value === 0 ){
+    if(inpBox.value === "" ){
         alert('Please Enter the link')
     }
     else{
         let URL = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${inpBox.value}`;
         Image.src = URL;
         DownloadBtn.style.display = "block";
-        DownloadBtn.href = URl;
-
+        // DownloadBtn.href = URL
+        let img = document.querySelector('.Image img')
+        let imgAtrr = img.getAttribute('src');
+        DownloadBtn.setAttribute("href", imgAtrr);
     }
 }
 
+
+
 genBtn.addEventListener('click', generateQr);
-genBtn.addEventListener('enter', generateQr);
