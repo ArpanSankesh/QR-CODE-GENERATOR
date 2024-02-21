8b789a2f547982bbaab4600397a37dcc13303013
ArpanSankesh <arpansankesh830@gmail.com>
2024-02-21 21:00:01 +0530

    finnal touches

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 let inpBox = document.querySelector('#inpBox');
 let genBtn = document.querySelector('.genBtn');
+let DownloadBtn = document.querySelector('.downloadBtn');
 let Image = document.querySelector('#Image');
 
 
+
 let generateQr = function (){
-    alert('working')
+    if(inpBox.value === 0 ){
+        alert('Please Enter the link')
+    }
+    else{
+        let URL = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${inpBox.value}`;
+        Image.src = URL;
+        DownloadBtn.style.display = "block";
+        DownloadBtn.href = URl;
+
+    }
 }
 
 genBtn.addEventListener('click', generateQr);
+genBtn.addEventListener('enter', generateQr);
