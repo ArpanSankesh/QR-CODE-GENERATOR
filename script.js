6739e50df4e62e3bc362d6d7417adb1905cce1f6
ArpanSankesh <arpansankesh830@gmail.com>
2024-02-21 20:32:28 +0530

    design update

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-let inpBox = documet.querySelector('#inpBox');
+let inpBox = document.querySelector('#inpBox');
+let genBtn = document.querySelector('.genBtn');
+let Image = document.querySelector('#Image');
+
+
+let generateQr = function (){
+    alert('working')
+}
+
+genBtn.addEventListener('click', generateQr);
